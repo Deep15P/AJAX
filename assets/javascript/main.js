06ab36a2031618440cd52664f1b571aa093b9421
Planet15 <parmar0815@gmail.com>
2019-03-11 17:16:11 -0400

    got the gifs to show up on the page. Also got the submit button working, where a user can type in a new search.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,37 +4,66 @@ $(document).ready(function(){
 
     // startButtons();
 
-    function startButtons(arrayAdd, classAdd, placeToPut) {
+    function startButtons() {
+        $("#buttons").empty();
 
-        for (let i = 0; i < arrayAdd.length; i++) {
+        for (let i = 0; i < nbaArray.length; i++) {
             var btn = $("<button>");
-            btn.addClass(classAdd); 
-            btn.attr("data-name", arrayAdd[i]);
-            btn.text(arrayAdd[i]);
-            $(placeToPut).append(btn);
+            btn.addClass("nbaClass"); 
+            btn.attr("data-name", nbaArray[i]);
+            btn.text(nbaArray[i]);
+            $("#buttons").append(btn);
         }
     };
 
-$(document).on("click", ".gif-button", function() {
+    $("#buttonToClick").on("click", function(event){
+        event.preventDefault();
+        var addedData = $("#userChoice").val().trim();
+        if (addedData != "") {
+            nbaArray.push(addedData);
+            startButtons();
+            $("#userChoice").val();
+        }
+    })
+
+$(document).on("click", ".nbaClass", function() {
 
         var nba = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ZBwLge5PL0tz4xUHC7ZJGqJ6ahDqlzne&q=" + nba + "&limit=10&offset=0&rating=G&lang=en";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + nba + "&rating=g&limit=10&api_key=ZBwLge5PL0tz4xUHC7ZJGqJ6ahDqlzne";;
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response){
-
+            console.log(response);
            
             // look at the response in the console 
             // create a variable called rating and save the response.rating 
             // append it in the id of "#gifImages"
             var results = response.data;
-            console.log(response);
+            
+
+            for (var i=0; i < results.length; i++) {
+                var dataImage = $("<img>");
+                dataImage.attr("src", results[i].images.fixed_height_still.url);
+                dataImage.attr("data-animate", results[i].images.fixed_height.url);
+                dataImage.attr("data-still", results[i].images.fixed_height_still.url);
+                dataImage.addClass("gif");
+                dataImage.attr("data-state", "still");
+
+                var newItemdiv = $('<div class="newItem">');
+                var gifRating = results[i].rating;
+                var divRating = $("<p>").text("Rating: " + gifRating);
+
+                newItemdiv.append(divRating);
+                newItemdiv.append(dataImage);
+
+                $("#gifImages").prepend(newItemdiv);
+            }
         });
         
     });
-    startButtons( nbaArray, "gif-button", "#buttons");
+    startButtons( nbaArray, "#buttons");
     
 
 
